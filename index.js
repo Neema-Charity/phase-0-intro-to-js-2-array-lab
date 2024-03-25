9267c4704d1690df572323988b669a7072005331
Neema-Charity <ncharityyy@gmail.com>
2024-03-25 17:51:22 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat (name){
+    cats.push (name = "Ralph")
+}
+
+function destructivelyPrependCat (name){
+    cats.unshift (name = "Bob")
+}
+
+function destructivelyRemoveLastCat (name){
+    cats.pop ("Garfield")
+}
+
+function destructivelyRemoveFirstCat (name){
+    cats.shift("Milo")
+}
+
+function appendCat (Kayan){
+    return [...cats, Kayan]
+}
+
+function prependCat(Rafiki) {
+    return [Rafiki, ...cats]
+}
+
+function removeLastCat (name){
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat (name){
+    return cats.slice(1)
+
+}
+
+
+
